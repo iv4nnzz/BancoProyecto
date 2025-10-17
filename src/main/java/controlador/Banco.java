8f13489006c29b024535c8ce4433320a82060192
ivan <9spotzs@gmail.com>
2025-10-16 19:16:18 -0500

    Añadior a la clase Banco los atributos con array ademas del constructor

--- src/main/java/controlador/Banco.java
+++ src/main/java/controlador/Banco.java
@@ -3,11 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
+import modelo.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author 9spot
  */
 
 public class Banco {
-
+    private ArrayList<Cuenta> cuentas;
+    
+    public Banco() {
+        this.cuentas = new ArrayList<>();
+    }
 }
