19e943cec1c8a73049b5d4e0b4d1b66e1b235790
ivan <9spotzs@gmail.com>
2025-10-16 19:03:59 -0500

    Crear atributo para la clase CuentaCorriente ademas con su respectivo constructor usando Super

--- src/main/java/modelo/CuentaCorriente.java
+++ src/main/java/modelo/CuentaCorriente.java
@@ -8,6 +8,13 @@ package modelo;
  *
  * @author 9spot
  */
-public class CuentaCorriente  {
+public class CuentaCorriente extends Cuenta {
+    private double cupoSobregiro;
     
+    public CuentaCorriente(String numero, String nombreDueño, double saldo, 
+                          Fecha fechaApertura, double cupoSobregiro) {
+        super(numero, nombreDueño, saldo, fechaApertura);
+        this.cupoSobregiro = cupoSobregiro;
+    }
+  
 }
