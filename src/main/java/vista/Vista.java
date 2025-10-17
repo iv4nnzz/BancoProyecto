33f6a11ecce2a91e53a9e002c314bbabdc501253
ivan <9spotzs@gmail.com>
2025-10-16 19:24:11 -0500

    Añadir atributo de Banco, añadir el constructor para clase vista ademas del metodo principal del menu

--- src/main/java/vista/Vista.java
+++ src/main/java/vista/Vista.java
@@ -8,6 +8,51 @@ package vista;
  *
  * @author 9spot
  */
+import controlador.Banco;
+import modelo.*;
+import javax.swing.JOptionPane;
+import java.util.Calendar;
 
 public class Vista {
+    private Banco banco;
+    
+    public Vista() {
+        this.banco = new Banco();
+    }
+    
+    public void mostrarMenuPrincipal() {
+        boolean continuar = true;
+        
+        while (continuar) {
+            String[] opciones = {"Crear Cuenta", "Operaciones", "Listar Cuentas", "Salir"};
+            int seleccion = JOptionPane.showOptionDialog(
+                null,
+                "SISTEMA BANCARIO\nSeleccione una opción:",
+                "Menú Principal",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                opciones,
+                opciones[0]
+            );
+            
+            switch (seleccion) {
+                case 0:
+                    crearCuenta();
+                    break;
+                case 1:
+                    menuOperaciones();
+                    break;
+                case 2:
+                    mostrarMensaje(banco.listarCuentas());
+                    break;
+                case 3:
+                case -1: 
+                    continuar = false;
+                    mostrarMensaje("Gracias por usar el Sistema Bancario");
+                    break;
+            }
+        }
+    }
+
 }
